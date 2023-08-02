c35ac7f152ab93ef02cbce15e04797638cc12af2
Tony_Huang <Tony_Huang@TONY_HUANG666>
2023-08-02 22:24:56 +0800

    很莫名其妙，添加了避免丢失的代码以后vs code的就用不了了

---
+++ User/main.c
@@ -0,0 +1,45 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "Serial.h"
+#include "LED.h"
+#include "string.h"
+
+int main(void)
+{
+	OLED_Init();
+	LED_Init();
+	Serial_Initilize();
+	OLED_ShowString(1, 1, "TxPacket");
+	OLED_ShowString(3, 1, "RxPacket");
+	while (1)
+	{
+		if (Serial_RxFlag == 1)
+		{
+			OLED_ShowString(4, 1, "                ");
+			OLED_ShowString(4, 1, Serial_RxPackage);
+			
+			if (strcmp(Serial_RxPackage, "LED_ON") == 0)
+			{
+				LED1_ON();
+				Serial_SendString("LED_ON_OK\r\n");
+				OLED_ShowString(2, 1, "                ");
+				OLED_ShowString(2, 1, "LED_ON_OK");
+			}
+			else if (strcmp(Serial_RxPackage, "LED_OFF") == 0)
+			{
+				LED1_OFF();
+				Serial_SendString("LED_OFF_OK\r\n");
+				OLED_ShowString(2, 1, "                ");
+				OLED_ShowString(2, 1, "LED_OFF_OK");
+			}
+			else
+			{
+				Serial_SendString("ERROR\r\n");
+				OLED_ShowString(2, 1, "                ");
+				OLED_ShowString(2, 1, "ERROR");
+			}
+			Serial_RxFlag = 0;
+		}
+	}
+}
